472b6f8370477d0230c477437ef49b7cabba1889
Michael Zhu <mzhu23@gmail.com>
2020-10-23 17:17:56 -0700

    Modified index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby!" />
+      <p>What a world.</p>
+      <img src="https://sportshub.cbsistatic.com/i/r/2020/01/30/a30c32f1-86e4-48f1-82ba-913a4e2ff913/thumbnail/640x360/0cd73be80d2b3a7969468d7cceb4789b/lebron-kobe-1.jpg" alt="" />
+    </div>
+  );
 }
