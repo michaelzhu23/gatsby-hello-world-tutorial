de119fe317edd576ed307ae2893f574685d12254
Michael Zhu <mzhu23@gmail.com>
2020-10-23 19:54:13 -0700

    Modified home page in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Link to="/contact/">Contact</Link>
       <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
+      <p>Mamba Forever</p>
       <img src="https://sportshub.cbsistatic.com/i/r/2020/01/30/a30c32f1-86e4-48f1-82ba-913a4e2ff913/thumbnail/640x360/0cd73be80d2b3a7969468d7cceb4789b/lebron-kobe-1.jpg" alt="" />
     </div>
   );
